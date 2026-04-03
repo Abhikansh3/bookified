599393d8b72a1677f89adf07314cc55ba4fc42d1
Abhikansh Mittal <abhikansh33@gmail.com>
2026-04-03 13:27:57 +0530

    uploadform

--- app/(root)/books/new/page.tsx
+++ app/(root)/books/new/page.tsx
@@ -1,3 +1,5 @@
+import UploadForm from '@/components/UploadForm';
+
 const page = () => {
   return (
     <main className="wrapper container">
@@ -6,6 +8,7 @@ const page = () => {
           <h1 className="page-title-xl">Add a New Book</h1>
           <p className="subtitle">Upload a PDF to generate your interactive interview</p>
         </section>
+        <UploadForm />
       </div>
     </main>)
 }
